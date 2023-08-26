423ba27301d3452013ed727773dc7479bd3e2bac
Ebubekir Nazlı <101016149+Ebu13@users.noreply.github.com>
2023-08-26 18:18:58 +0300

    new

---
+++ firstapp/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input type="text" v-model="inputText" placeholder="Metin girin">
+    <Text :msg="inputText" />
+  </div>
+</template>
+
+<script>
+import Text from './components/Text.vue'
+
+export default {
+  components: {
+    Text,
+  },
+  data() {
+    return {
+      inputText: ''
+    }
+  }
+}
+</script>
